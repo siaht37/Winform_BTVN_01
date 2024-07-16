73ba39ca6fcc10dbca561309906c43735a08897f
Matontrungsinh <thaihoang03072002@gmail.com>
2024-07-16 15:56:13 +0700

    first commit

---
+++ BTVN_01/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class BTVN_01
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int targetNumber = random.Next(100, 999);
+        string targetString = targetNumber.ToString();
+        int maxAttempts = 7;
+
+        Console.WriteLine("------------Chuong trinh doan so!--------------");
+        Console.WriteLine("Vui long nhap 1 so tu 100 den 999");
+
+        for (int attempt = 1; attempt <= maxAttempts; attempt++)
+        {
+            Console.Write($"Nhap lan {attempt}: Doan di: ");
+            string guess = Console.ReadLine();
+
+            if (guess.Length != 3)
+            {
+                Console.WriteLine("Vui long nhap toi da 3 ky tu");
+                attempt--;
+                continue;
+            }
+
+            if (guess == targetString)
+            {
+                Console.WriteLine("Dung oyyyyyyy");
+                Console.ReadKey();
+                return;
+            }
+
+            string response = GetFeedback(targetString, guess);
+            Console.WriteLine($"Response: {response}");
+        }
+
+        Console.WriteLine($"Ban da nhap sai {maxAttempts} lan. So can doan la {targetString}.");
+        Console.ReadKey();
+    }
+
+    static string GetFeedback(string target, string guess)
+    {
+        string feedback = "";
+        for (int i = 0; i < target.Length; i++)
+        {
+            if (target[i] == guess[i])
+            {
+                feedback += "+";
+            }
+            else if (target.Contains(guess[i].ToString()))
+            {
+                feedback += "?";
+            }
+            else
+            {
+                feedback += "_";
+            }
+        }
+        return feedback;
+    }
+}
---
+++ Buoi1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Buoi1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Nhap so luong san pham: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                Console.Write("Nhap don gia cua san pham: ");
+                float price = float.Parse(Console.ReadLine());
+
+                float total = quantity * price;
+
+                Console.WriteLine($"Tong gia tri don hang la: {total:C}");
+                Console.ReadKey();
+            }
+             catch (FormatException ex)
+            {
+                Console.WriteLine("Vui long nhap so hop le");
+                Console.ReadKey();
+            }
+        }
+    }
+}
